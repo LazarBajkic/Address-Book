c10c425ad459cc6c9906dab001a4b41efd6f43cb
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-04-22 17:05:29 +0200

    Find option
    Added the option to find a contact by inserting their first name and email

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         String uname="root";
         String url="jdbc:mysql://localhost:3306/AddressBook";
         String password="Ceramida";
-        String query="SELECT * FROM Contacts";
+        String getContacts="SELECT * FROM Contacts";
                 
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
@@ -25,13 +25,12 @@ public class Main {
         boolean checkTrue = true;
       
         System.out.println("Hello and welcome to your address book!");
-        System.out.println("What would you like to do?\ncheck current contacts,add a contact,delete a contact\n1.add\n2.check\n3.delete\n4.exit");
+        System.out.println("What would you like to do?\ncheck current contacts,add a contact,delete a contact or find a contact\n1.add\n2.check\n3.delete\n4.find\n5.exit");
     
         while(checkTrue==true){
         
         Scanner s = new Scanner(System.in);
         String choice=s.nextLine();
-
         if(choice.equals("add")){
             System.out.println("please input the necessary information: \nFirst name: ");
             String firstName=s.nextLine();
@@ -50,33 +49,52 @@ public class Main {
         }
 
         if(choice.equals("check")){
-            ResultSet result=statement.executeQuery(query);
+            ResultSet result=statement.executeQuery(getContacts);
             String contactData="";
             while(result.next()){
                 for(int i = 1;i<=5;i++){
-                    contactData += result.getString(i) + "   \n";
+                    contactData +="\n---" + result.getString(i);
+                    
                 }
+               
             }
+            System.out.println("---------------------");
             System.out.println(contactData);
-            
+            System.out.println("---------------------");
         } 
         
         if(choice.equals("delete")){
             System.out.println("insert the first name of the person which you want to delete: ");
             String delFirstName = s.nextLine();
-            System.out.println("Now insert the last name of that person: ");
-            String delLastName = s.nextLine();
-            String delContact="Delete from contacts where first_name='"+delFirstName+"'AND last_name='"+delLastName+"'";
+            System.out.println("Now insert the address of that person: ");
+            String delAddress = s.nextLine();
+            String delContact="Delete from contacts where first_name='"+delFirstName+"'AND address='"+delAddress+"'";
             statement.executeUpdate(delContact);
         }
+
+        if(choice.equals("find")){
+            System.out.println("insert the first name of the person which you want to find: ");
+            String findFirstName = s.nextLine();
+            System.out.println("Now insert the email address of that person: ");
+            String findEmail = s.nextLine();
+            String findContact="Select * from contacts where first_name='"+findFirstName+"'AND email='"+findEmail+"'";
+            ResultSet result = statement.executeQuery(findContact);
+            String find = "";
+            while(result.next()){
+                for(int i = 1;i<=5;i++){
+                find += result.getString(i) + "\n";
+            }
+        }
+            System.out.println(find);
+        }
+
+        System.out.println("what would you like to do now?");
         
         if(choice.equals("exit")){
             checkTrue=false;
             s.close();
         }
        
-        System.out.println("what would you like to do now?");
-
     }
 }
 }
